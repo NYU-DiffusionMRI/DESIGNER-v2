90d2ca50753822cf1ad4b221ad3ccebc3bdac507
Benjamin Aron <benaron@benjamins-mbp.policytech.com>
2023-10-06 12:48:39 -0400

    versioning

--- designer2/designer.py
+++ designer2/designer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = "0.0.9"
+__version__ = "2.0.0"
 
 from lib.designer_input_utils import *
 from lib.designer_func_wrappers import *
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ subprocess.run(['./rpg_cpp/compile.sh'], shell=True)
 
 setup(
         name ='designer2',
-        version ='0.0.33',
+        version ='2.0.0',
         author ='Benjamin Ades-Aron',
         author_email ='benjamin.ades-aron@nyulangone.org',
         url ='https://github.com/badesar1/designer_v2_dev.git',
