7158e7c0817bf9a6bae94923749a9efccaea2f11
dongYoun2 <dlehddbs6425@gmail.com>
2025-06-08 03:28:32 -0400

    move FFTW build to Dockerfile and remove pre-built FFTW

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,63 @@
 # Use an official Python runtime as a parent image
-FROM python:3.12.4-bookworm
+FROM python:3.12.4-bookworm AS base
 
-# Multi stage build from existing dependencies
+# Install common dependencies
+RUN apt-get -qq update \
+    && apt-get install -yq --no-install-recommends \
+    libgl1-mesa-dev \
+    cmake \
+    make \
+    gcc \
+    g++ \
+    wget \
+    hdf5-tools \
+    libhdf5-serial-dev \
+    liblapack-dev \
+    libopenblas-dev \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
+
+# FFTW build stage
+FROM base AS fftw-builder
+
+
+WORKDIR /tmp
+# Download FFTW (this layer will be cached unless the URL changes)
+RUN wget http://www.fftw.org/fftw-3.3.10.tar.gz \
+    && tar xzf fftw-3.3.10.tar.gz \
+    && cd fftw-3.3.10 \
+    && ./configure \
+        --enable-threads \
+        --disable-doc \
+        --enable-sse2 \
+        --enable-avx \
+        CFLAGS="-O3" \
+    && make -j$(nproc) \
+    && make install \
+    && cd / \
+    && rm -rf /tmp
+
+# Final stage
+FROM base
+# Copy dependencies from other images
 COPY --from=twom/fsl:6.0 /usr/local/fsl /usr/local/fsl
 COPY --from=twom/mrtrix3:dev-latest /usr/local/mrtrix3/build /usr/local/mrtrix3_build
 COPY --from=twom/ants:v2.5.4 /usr/local/ants /usr/local/ants
 
+# Copy FFTW files from builder stage
+COPY --from=fftw-builder /usr/local/lib/libfftw* /usr/local/lib/
+COPY --from=fftw-builder /usr/local/include/fftw3* /usr/local/include/
 
-RUN apt-get -qq update && \
-    apt-get install -yq --no-install-recommends \
-    cmake \
-    g++ \
-    gcc \
-    hdf5-tools \
-    libgl1-mesa-dev \
-    libhdf5-serial-dev \
-    liblapack-dev \
-    libopenblas-dev \
-    pkg-config && \
-    rm -rf /var/lib/apt/lists/*
+# Verify FFTW installation and set up library cache
+RUN ldconfig \
+    && ldconfig -p | grep fftw
 
+# Install Python dependencies
 COPY requirements.txt .
-# Install any needed packages specified in requirements.txt
 RUN pip install -r requirements.txt --no-cache-dir
-
-
 
 # Set the working directory in the container to /app
 WORKDIR /app
-
 
 # Add the current directory contents into the container at /app
 ADD . /app
@@ -36,10 +65,9 @@ ADD . /app
 # Run setup.py
 RUN python -m pip install .
 
-
 ENV FSLDIR=/usr/local/fsl
 ENV FSLOUTPUTTYPE=NIFTI_GZ
 ENV PATH="${PATH}:/usr/local/fsl/bin:/usr/local/mrtrix3_build/bin:/usr/local/ants/bin"
-ENV LD_LIBRARY_PATH="/usr/local/mrtrix3_build/src:/usr/local/mrtrix3_build/core:/usr/local/ants/lib:/app/rpg_cpp/fftw-3.3.10/build/lib"
+ENV LD_LIBRARY_PATH="/usr/local/mrtrix3_build/src:/usr/local/mrtrix3_build/core:/usr/local/ants/lib:/usr/local/lib"
 ENV PYTHONPATH="/usr/local/mrtrix3_build/lib"
 RUN echo ". /usr/local/fsl/etc/fslconf/fsl.sh" >> /root/.bashrc
--- setup.py
+++ setup.py
@@ -1,56 +1,22 @@
 import os
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
-import subprocess
 from glob import glob
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-long_description = '''designer and TMI package for use with brain 
-    diffusion MRI processing. Designer is used for image preprocessing 
-    including denoising, partial-fourier gibbs correction, epi eddy 
+long_description = '''designer and TMI package for use with brain
+    diffusion MRI processing. Designer is used for image preprocessing
+    including denoising, partial-fourier gibbs correction, epi eddy
     current and motion, and normalization. TMI is used for dti/dki/wmti/smi
     along with outlier correction.'''
 
 class CustomBuildExt(build_ext):
-    def run(self):
-        self.clean_fftw()  # Ensure previous builds are cleaned
-        self.build_fftw()
-        super().run()
-
-    def clean_fftw(self):
-        fftw_source_dir = os.path.abspath("rpg_cpp/fftw-3.3.10")
-        fftw_build_dir = os.path.join(fftw_source_dir, "build")
-
-        if os.path.exists(fftw_build_dir):
-            subprocess.check_call(["make", "clean"], cwd=fftw_source_dir)
-
-    def build_fftw(self):
-        fftw_source_dir = os.path.abspath("rpg_cpp/fftw-3.3.10")  # Adjusted path
-        fftw_build_dir = os.path.join(fftw_source_dir, "build")
-
-        if not os.path.exists(fftw_build_dir):
-            os.makedirs(fftw_build_dir)
-
-        env = os.environ.copy()
-        env['CFLAGS'] = '-fPIC'
-        subprocess.check_call(
-            ["./configure", "--prefix=" + fftw_build_dir, "--enable-shared", "--disable-doc", "--enable-threads"],
-            #["./configure", "--prefix=" + fftw_build_dir, "--enable-shared", "--disable-doc"],
-            cwd=fftw_source_dir,
-            env=env
-        )
-        subprocess.check_call(["make"], cwd=fftw_source_dir)
-        subprocess.check_call(["make", "install"], cwd=fftw_source_dir)
-
-        self.fftw_include_dir = os.path.join(fftw_build_dir, "include")
-        self.fftw_lib_dir = os.path.join(fftw_build_dir, "lib")
-
     def build_extension(self, ext):
-        ext.include_dirs.append(self.fftw_include_dir)
-        ext.library_dirs.append(self.fftw_lib_dir)
-        #ext.extra_link_args = ["-Wl,-rpath," + self.fftw_lib_dir]
+        # Use system FFTW installation
+        ext.include_dirs.append('/usr/local/include')
+        ext.library_dirs.append('/usr/local/lib')
         super().build_extension(ext)
 
 class get_pybind_include:
@@ -66,13 +32,12 @@ ext_modules = [
         ["rpg_cpp/unring_rpg_pybind.cpp"],
         include_dirs=[
             get_pybind_include(),
-            # FFTW include directory will be added during the build
+            '/usr/local/include',  # System FFTW headers
         ],
         library_dirs=[
-            # FFTW library directory will be added during the build
+            '/usr/local/lib',  # System FFTW libraries
         ],
-        #libraries=["fftw3","fftw3_threads"],
-        libraries=["fftw3","fftw3_threads","m"],
+        libraries=["fftw3", "fftw3_threads", "m"],
         extra_compile_args=["-std=c++11"],
     ),
 ]
@@ -109,4 +74,3 @@ setup(
         zip_safe = False,
         verbose = True,
 )
-    
