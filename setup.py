df3b278adee240d010f40a7de7af2414b2325ed9
dongYoun2 <dlehddbs6425@gmail.com>
2026-01-06 22:35:13 +0000

    update dependency managing

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 import os
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-from glob import glob
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-long_description = '''designer and TMI package for use with brain
-    diffusion MRI processing. Designer is used for image preprocessing
-    including denoising, partial-fourier gibbs correction, epi eddy
-    current and motion, and normalization. TMI is used for dti/dki/wmti/smi
-    along with outlier correction.'''
+# Dependencies are now defined in pyproject.toml
+# This setup.py only handles the C++ extension
 
 class CustomBuildExt(build_ext):
     def build_extension(self, ext):
@@ -43,34 +36,6 @@ ext_modules = [
 ]
 
 setup(
-        name ='designer2',
-        version ='2.0.15',
-        author ='Benjamin Ades-Aron',
-        author_email ='benjamin.ades-aron@nyulangone.org',
-        url ='https://github.com/NYU-DiffusionMRI/DESIGNER-v2',
-        description ='designerV2',
-        long_description = long_description,
-        long_description_content_type ="text/markdown",
-        license ='NYU',
-        packages = find_packages(),
-        data_files=[('constant', glob('constant/*'))],
-        include_package_data=True,
-        entry_points = {
-            'console_scripts': [
-                'designer = designer2.designer:main',
-                'tmi = designer2.tmi:main'
-            ]
-        },
-        classifiers = [
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved",
-            "Operating System :: OS Independent",
-        ],
-        ext_modules = ext_modules,
-        cmdclass={"build_ext": CustomBuildExt},
-        keywords = 'python diffusion mri mppca gibbs preprocessing dki smi DESIGNER TMI',
-        python_requires = '>=3.9',
-        install_requires = requirements,
-        zip_safe = False,
-        verbose = True,
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": CustomBuildExt},
 )
